83c5003dbd4a1c12439b57048b2e6a12fe648d5b
gilmar <jiraspiom@yahoo.com.br>
2018-09-03 16:22:51 -0300

    criado a funcao de do botao volume, para que o mesmo ache o volume atual e que possa alterar o volume de media

--- app/src/main/java/br/com/example/alalgi/mediaplayer/MainActivity.java
+++ app/src/main/java/br/com/example/alalgi/mediaplayer/MainActivity.java
@@ -1,15 +1,20 @@
 package br.com.example.alalgi.mediaplayer;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private Button buttonPlay, buttonPause, buttonStop;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        inicializarSeekBar();
 
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonPause = findViewById(R.id.buttonPause);
@@ -46,6 +52,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void inicializarSeekBar(){
+        seekVolume = findViewById(R.id.seekVolume);
+
+        //configurando o audio manager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //recupero o valor do volume maximo atual
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //configura o valor maximo da seek bar
+        seekVolume.setMax(volumeMaximo);
+
+        //configura o valor atual da seekbar
+        seekVolume.setProgress(volumeAtual);
+
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
 
     public void executaSom(){
         if (mediaPlayer != null) {
